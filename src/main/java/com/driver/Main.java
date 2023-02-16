18325196dc7b148626a17cc637cb3680ef8cd133
Adesh <adeshsamdadiya@gmail.com>
2023-02-17 00:00:16 +0530

    Solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+
+
+    public static void main(String[] args){
+        RWOnly a = new RWOnly();
+        a.setName("Twinkle");
+        System.out.println(a.getName());
+    }
 }
